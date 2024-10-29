14b144d3df5cbca633be6c3ae7da5353b2e9242d
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-29 03:16:14 -0300

    Refactor Webpack Encore configuration and assets paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
-// TrytonBot/webpack.config.js
-
 const Encore = require('@symfony/webpack-encore');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-
 Encore
-    .addStyleEntry('app', './assets/styles/app.scss') // Ruta corregida y verificada para los estilos
-    .addEntry('appJs', './assets/app.js') // Entrada de JavaScript
+    .addStyleEntry('app', './assets/styles/app.scss')
+    .addEntry('appJs', './assets/app.js')
     .setOutputPath('public/build/')
-    .setPublicPath('/build')
+    .setPublicPath('/TrytonBot/public/build') // Ajuste de la ruta
     .enableStimulusBridge('./assets/controllers.json')
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
@@ -24,7 +21,5 @@ Encore
         config.corejs = '3.23';
     })
     .enableSassLoader();
-
-;
 
 module.exports = Encore.getWebpackConfig();
