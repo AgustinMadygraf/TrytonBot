658cd6c6dcf555be5d270edb2c15e0a258c3c3e7
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-30 02:34:58 -0300

    Add MonologBundle for enhanced logging and configure logging settings

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,10 @@
 <?php
-// TrytonBot/config/bundles.php
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
