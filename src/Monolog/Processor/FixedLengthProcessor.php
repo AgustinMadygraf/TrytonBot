b2a38b95c45a43ef32b45f08cc2f351050f7f165
AgustinMadygraf <agustin.mtto.madygraf>
2024-10-31 14:55:07 -0300

    Update FixedLengthProcessor to adjust channel level name padding to 12 characters

--- src/Monolog/Processor/FixedLengthProcessor.php
+++ src/Monolog/Processor/FixedLengthProcessor.php
@@ -8,9 +8,9 @@ class FixedLengthProcessor
 {
     public function __invoke(array $record): array
     {
-        // Aseguramos que el campo channel.level_name tenga exactamente 15 caracteres
+        // Aseguramos que el campo channel.level_name tenga exactamente 12 caracteres
         $channelLevel = $record['channel'] . '.' . $record['level_name'];
-        $record['extra']['channel_level'] = str_pad($channelLevel, 15, ' ', STR_PAD_RIGHT);
+        $record['extra']['channel_level'] = str_pad($channelLevel, 12, ' ', STR_PAD_RIGHT);
 
         return $record;
     }
