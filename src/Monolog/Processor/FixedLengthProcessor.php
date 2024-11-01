ce723517dfe97c974ef1c0536db40ec3ece7fdab
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-01 10:14:17 -0300

    Refactor FixedLengthProcessor to format file and line information; update Monolog line formatter for improved log structure

--- src/Monolog/Processor/FixedLengthProcessor.php
+++ src/Monolog/Processor/FixedLengthProcessor.php
@@ -8,9 +8,19 @@ class FixedLengthProcessor
 {
     public function __invoke(array $record): array
     {
-        // Aseguramos que el campo channel.level_name tenga exactamente 12 caracteres
+        // Formatear cada campo con una longitud fija
+        $record['extra']['index'] = str_pad($record['extra']['index'], 3, ' ', STR_PAD_RIGHT);
+        
         $channelLevel = $record['channel'] . '.' . $record['level_name'];
         $record['extra']['channel_level'] = str_pad($channelLevel, 12, ' ', STR_PAD_RIGHT);
+
+        if (isset($record['extra']['file']) && isset($record['extra']['line'])) {
+            $fileLine = sprintf('%s:%s', $record['extra']['file'], $record['extra']['line']);
+            $record['extra']['file_line'] = str_pad(substr($fileLine, 0, 22), 22, ' ', STR_PAD_RIGHT);
+        }
+
+        $record['message'] = str_pad(substr($record['message'], 0, 60), 60, ' ', STR_PAD_RIGHT);
+        $record['context'] = str_pad(substr(json_encode($record['context']), 0, 80), 80, ' ', STR_PAD_RIGHT);
 
         return $record;
     }
